ea6a1fbba64338ba48af009a44a920dd76b14920
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-24 14:49:12 -0400

    Update Likes

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,6 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 
 function ToyCard({ toy: { id, name, image, likes }, onDeleteToy }) {
+  const [toyLikes, setToyLikes] = useState(likes)
+
+  function handleLikes() {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ likes: toyLikes + 1 }),
+    })
+      .then((response) => response.json())
+      .then((likeData) => setToyLikes(likeData.likes))
+      .catch((error) => console.error("Fetch error:", error))
+  }
+
   function handleToyDelete() {
     fetch(`http://localhost:3001/toys/${id}`, {
       method: "DELETE",
@@ -14,8 +29,10 @@ function ToyCard({ toy: { id, name, image, likes }, onDeleteToy }) {
     <div className="card">
       <h2>{name}</h2>
       <img src={image} alt={name} className="toy-avatar" />
-      <p>{likes} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
+      <p>{toyLikes} Likes </p>
+      <button className="like-btn" onClick={handleLikes}>
+        Like {"<3"}
+      </button>
       <button className="del-btn" onClick={handleToyDelete}>
         Donate to GoodWill
       </button>
