eb88b50febdb687aa878f7a5d37a79f5ccf79bd5
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-24 14:27:10 -0400

    Delete toy

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,13 +1,24 @@
 import React from "react"
 
-function ToyCard({ toy: { id, name, image, likes } }) {
+function ToyCard({ toy: { id, name, image, likes }, onDeleteToy }) {
+  function handleToyDelete() {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .catch((error) => console.error("Fetch error:", error))
+    onDeleteToy(id)
+  }
+
   return (
     <div className="card">
       <h2>{name}</h2>
       <img src={image} alt={name} className="toy-avatar" />
       <p>{likes} Likes </p>
       <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <button className="del-btn" onClick={handleToyDelete}>
+        Donate to GoodWill
+      </button>
     </div>
   )
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,9 @@
 import React from "react"
 import ToyCard from "./ToyCard"
 
-function ToyContainer({ toys }) {
+function ToyContainer({ toys, onDeleteToy }) {
   const displayedToys = toys.map((toy) => {
-    return <ToyCard key={toy.id} toy={toy} />
+    return <ToyCard key={toy.id} toy={toy} onDeleteToy={onDeleteToy} />
   })
   return <div id="toy-collection">{displayedToys}</div>
 }
